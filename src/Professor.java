34915ee71578371ca0f4e3ca0246355d6da0dbe0
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-09-13 20:18:38 +0000

    Criar classe Professor

---
+++ src/Professor.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor {
+
+    private String nome;
+    private String cpf;
+    private List<Aluno> alunos;
+
+    public Professor(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.alunos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        if (aluno != null && !alunos.contains(aluno)) {
+            alunos.add(aluno);
+            aluno.setProfessorResponsavel(this);
+        }
+    }
+}
