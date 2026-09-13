e0ff37a52c025725c3d420182e6f730fc533ebf2
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-09-13 20:18:54 +0000

    Criar classe ExercicioTreino

---
+++ src/ExercicioTreino.java
@@ -0,0 +1,24 @@
+public class ExercicioTreino {
+
+    private Exercicio exercicio;
+    private int series;
+    private int repeticoes;
+
+    public ExercicioTreino(Exercicio exercicio, int series, int repeticoes) {
+        this.exercicio = exercicio;
+        this.series = series;
+        this.repeticoes = repeticoes;
+    }
+
+    public Exercicio getExercicio() {
+        return exercicio;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public int getRepeticoes() {
+        return repeticoes;
+    }
+}
