d1904e1e9a5409973205d1a000c52f7728d61a2d
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-09-13 20:18:05 +0000

    Criar classe Aluno

---
+++ src/Aluno.java
@@ -0,0 +1,44 @@
+public class Aluno {
+
+    private String nome;
+    private String cpf;
+    private Professor professorResponsavel;
+    private FichaTreino fichaTreino;
+
+    public Aluno(String nome, String cpf) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("O nome do aluno é obrigatório.");
+        }
+
+        if (cpf == null || cpf.isBlank()) {
+            throw new IllegalArgumentException("O CPF do aluno é obrigatório.");
+        }
+
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Professor getProfessorResponsavel() {
+        return professorResponsavel;
+    }
+
+    public void setProfessorResponsavel(Professor professorResponsavel) {
+        this.professorResponsavel = professorResponsavel;
+    }
+
+    public FichaTreino getFichaTreino() {
+        return fichaTreino;
+    }
+
+    public void setFichaTreino(FichaTreino fichaTreino) {
+        this.fichaTreino = fichaTreino;
+    }
+}
