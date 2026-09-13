30ac2274bc0a43ec637ca09831820a2d9a90be89
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-09-13 20:18:22 +0000

    Criar classe Exercicio

---
+++ src/Exercicio.java
@@ -0,0 +1,18 @@
+public class Exercicio {
+
+    private String nome;
+    private String descricao;
+
+    public Exercicio(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
